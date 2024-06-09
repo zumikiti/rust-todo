dbf5b86a61a0d2dd6824b9dadb93b2458f77be26
akihiro <aponmaru@gmail.com>
2024-06-09 17:05:20 +0900

    modify: はじめに、 action を指定するように変更

--- src/main.rs
+++ src/main.rs
@@ -25,26 +25,24 @@ fn main() {
     show_all_todo(&todo_list);
 
     loop {
-        println!("Please input your todo.");
+        println!("Please input action.");
 
-        let mut todo = String::new();
+        let mut action = String::new();
 
         io::stdin()
-            .read_line(&mut todo)
+            .read_line(&mut action)
             .expect("Failed to read line");
 
-        todo = match todo.trim().parse() {
+        action = match action.trim().parse() {
             Ok(string) => string,
             Err(_) => continue,
         };
 
-        let num: i64 = (todo_list.len() +1).try_into().unwrap();
-
-        todo_list.push(Todo {
-            uid: num,
-            value: todo,
-            done: false,
-        });
+        if action == String::from("add") {
+            todo_list = add_todo(todo_list);
+        } else {
+            println!("non match action.")
+        }
 
         show_all_todo(&todo_list);
     }
@@ -57,4 +55,31 @@ fn show_all_todo(todos: &Vec<Todo>) {
         let done_text = if todo.done { "完了" } else { "未完了" };
         println!("ID:{} | {} ({})", todo.uid, todo.value, done_text);
     }
+}
+
+fn add_todo(mut todo_list: Vec<Todo>) -> Vec<Todo> {
+    println!("Please input new todo.");
+
+    let mut todo = String::new();
+
+    io::stdin()
+        .read_line(&mut todo)
+        .expect("Failed to read line");
+
+    todo = match todo.trim().parse() {
+        Ok(string) => string,
+        Err(_) => {
+            return todo_list;
+        },
+    };
+
+    let num: i64 = (todo_list.len() +1).try_into().unwrap();
+
+    todo_list.push(Todo {
+        uid: num,
+        value: todo,
+        done: false,
+    });
+
+    return todo_list;
 }
